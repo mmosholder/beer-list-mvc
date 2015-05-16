14fd5eb154fcf564f8fda159b8a50a93789cbb4b
Malinda Mosholder <m.m.mosholder@gmail.com>
2015-05-16 14:16:58 -0600

    initial small scaffolding of project, adding to-do lists and fleshing out functionality and features

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/beer-project');
+
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+
+var server = app.listen(9945, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
